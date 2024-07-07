ad25fcff899c5233e97d2f7f9cbc1397160ed11c
RajBhut214 <127262686+RajBhut@users.noreply.github.com>
2024-07-07 11:10:45 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@
 
     const io = new Server(server, {
         cors: {
-          origin: ["*"], 
+          origin: ["https://soticktack.vercel.app/", "https://soticktack.vercel.app/game"], 
           methods: ["GET", "POST"],
         },
       });
